56d83e735f5197fbca9c1a8a0a84b95192c03483
Frédéric Péters <fpeters@entrouvert.com>
2013-07-21 17:51:19 +0200

    use icons for top three menu items

--- src/pfwbged/theme/diazo_resources/static/main.js
+++ src/pfwbged/theme/diazo_resources/static/main.js
@@ -4,6 +4,11 @@
 /*global jQuery:false, document:false, window:false, location:false */
 
 $(function() {
+  /* highlight index icon when the member icon is selected, as that one is
+   * hidden; this should probably be done earlier, when producing the HTML. */
+  if ($('#portaltab-mystuff.selected').length == 1) {
+     $('#portaltab-index_html').addClass('selected');
+  }
   $('#portal-down a.open').click(function() {
     if ($('#favorites ul').length == 0) {
       $('#favorites').load('quicklinks');
