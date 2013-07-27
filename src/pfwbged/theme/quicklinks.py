a8cb1181abba2f4c16535b87090479b9b1e87f14
Vincent Fretin <vincent.fretin@gmail.com>
2013-07-27 09:44:53 +0200

    Fix Member folder link appearing for Anonymous and use base_hasattr to be safe

--- src/pfwbged/theme/quicklinks.py
+++ src/pfwbged/theme/quicklinks.py
@@ -1,8 +1,11 @@
 from five import grok
 from zope.interface import Interface
+
+from Products.CMFPlone.utils import base_hasattr
 from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
 
 import plone.api
+
 
 class QuickLinksView(grok.View):
     grok.context(Interface)
@@ -11,15 +14,32 @@ class QuickLinksView(grok.View):
 
     view_template = ViewPageTemplateFile('quicklinks_templates/view.pt')
 
+    def update(self):
+        self.member_folder = self._get_member_folder()
+
     def render(self):
         self.request.response.setHeader('Cache-Control', 'no-cache')
         return self.view_template()
 
-    def get_home_url(self):
+    def _get_member_folder(self):
         current = plone.api.user.get_current()
+        # When Anonymous current.id == 'acl_users' and current.getId() is None
+        current_id = current.getId()
+        if current_id is None:
+            return None
+
         members_folder = getattr(plone.api.portal.get(), 'Members')
-        if hasattr(members_folder, current.id):
-            return getattr(members_folder, current.id).absolute_url()
+        if base_hasattr(members_folder, current_id):
+            member_folder = getattr(members_folder, current_id)
+            return member_folder
+
+        return None
+
+    def get_home_url(self):
+        member_folder = self.member_folder
+        if member_folder is not None:
+            return member_folder.absolute_url()
+
         return None
 
     def get_global_collections(self):
@@ -30,12 +50,11 @@ class QuickLinksView(grok.View):
         return results
 
     def get_user_collections(self):
-        members_folder = getattr(plone.api.portal.get(), 'Members')
-        current = plone.api.user.get_current()
-        if not hasattr(members_folder, current.id):
+        member_folder = self.member_folder
+        if member_folder is None:
             return None
-        current_member_folder = getattr(members_folder, current.id)
-        results = list(current_member_folder.getFolderContents(
+
+        results = list(member_folder.getFolderContents(
             contentFilter={'portal_type': 'pfwbgedcollection',
                 'sort_on': 'sortable_title'}))
         return results
